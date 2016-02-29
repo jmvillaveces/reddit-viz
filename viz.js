5d4334e80bb3c83bd384f11f67414a6dbb20dfac
jmvillaveces <secevalliv@gmail.com>
2016-02-29 20:22:37 +0100

    working version with qtip

--- viz.js
+++ viz.js
@@ -1,7 +1,7 @@
 function main(){
     
     var stories = {};
-    var n = 2000;
+    var n = 3000;
     var events = [];
     var initial = true;
 
@@ -12,7 +12,6 @@ function main(){
     function handleRedditData(rawData) {
         var newData = formatSingleSubreddit(rawData);
         visualize(newData);
-        
     }
     
     function fetch(url, cb, n){
@@ -20,74 +19,86 @@ function main(){
         d3.json(url, function(error, data){
             if (error) return console.warn(error);
             cb(data);
-            console.log('Got data!!');
         });
         
         setTimeout(fetch, n, url, cb, n);
     };
     
-    //fetch( proxy + reddit_url, handleRedditData, n);
+    fetch( proxy + reddit_url, handleRedditData, n);
 }
+
+// Initialize tooltip
+var tooltip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.title; }).direction('se').offset([0, 3]);
 
 function formatSingleSubreddit(rawData) {
-    return _.pluck(rawData.data.children, "data")
+    return _.pluck(rawData.data.children, "data");
 }
 
-function visualize(redditActivitySources) {
-    
+function visualize(redditActivitySources){
     //Viz options
     var width = 500, barHeight = 20;
     
-    data = _.sortBy(redditActivitySources, function(d) { return - d.score });
+    data = _.sortBy(redditActivitySources, function(d) { return -d.score });
     
     //var scale 
     var scale = d3.scale.linear()
         .domain([0, d3.max(data, function(d){ return d.score; })])
         .range([0, width]);
     
-    console.log(scale.domain());
-    console.log(scale.range());
-    
     var chart = d3.select("#viz")
         .attr("width", width)
         .attr("height", barHeight * data.length);
     
-    
     // DATA JOIN
     // Join new data with old elements, if any.
-    var bar = chart.selectAll("g")
-        .data(data);
+    var bar = chart.selectAll("rect")
+        .data(data, function(d){return d.id;});
+    
+    var text = chart.selectAll("text")
+        .data(data, function(d){return d.id;});
     
     //UPDATE
     // Update old elements as needed.
-    chart.selectAll("rect")
-        .attr("width", function(d){ return console.log(d); scale(d.score); });
+    bar.transition()
+      .duration(750)
+        .attr("width", function(d){ return scale(d.score); })
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
     
-    console.log('text', chart.selectAll("text"));
-    chart.selectAll("text")
-        .attr("x", function(d) { return scale(d.score) - 15; })
-        .text(function(d) {return d.score; });
+    text.text(function(d) {return d.score; })
+        .attr("x", function(d) { return scale(d.score) - 5; })
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
     
     // ENTER
     // Create new elements as needed.
-    var g = bar.enter().append("g")
-            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    bar.enter().append("rect")
+            .attr("width", 0)
+            .attr("height", barHeight - 1)
+            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; })
+            .on('mouseover', tooltip.show)
+            .on('mouseout', tooltip.hide)
+        .transition()
+          .duration(750)
+            .attr("width", function(d){ return scale(d.score); });
     
-    g.append("rect")
-            .attr("width", function(d){ return scale(d.score); })
-            .attr("height", barHeight - 1);
-    
-    g.append("text")
-            .attr("x", function(d) { return scale(d.score) - 15; })
+    text.enter().append("text")
+            .attr("x", function(d) { return scale(d.score) - 5; })
             .attr("y", barHeight / 2)
             .attr("dy", ".35em")
+            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; })
             .text(function(d) {return d.score; });
     
     // EXIT
     // Remove old elements as needed.
-    bar.exit().remove();
+    bar.exit().transition()
+      .duration(325)
+      .style("fill-opacity", 1e-6)
+      .remove();
+    
+     text.exit().remove();
+    
+    // Invoke the tip in the context the visualization
+    bar.call(tooltip)
 }
-
 
 main();
 
